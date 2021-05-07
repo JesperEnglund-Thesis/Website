d1da9e6c67bc1623c3eae21753494598ebe79a0d
Jesper Englund <jesper.englund92@gmial.com>
2021-05-07 09:36:05 +0200

    more ws stuff

--- main.js
+++ main.js
@@ -366,7 +366,8 @@ map.on('load', function () {
               'coordinates': [16.15390, 58.62239]
             },
             'properties': {
-              'title': 'WS Nkpg'
+              'title': 'Norrköping Service Center',
+              'description':'<p><img src="images/wsdesc.png" style="width:100%"/></p>'
             }
           }
         ]
@@ -381,18 +382,29 @@ map.on('load', function () {
       'layout': {
           'icon-image': 'custom-marker',
           'icon-size': 0.7,
+          'icon-allow-overlap': true,
           // get the title name from the source's "title" property
-          'text-field': ['get', 'title'],
-          'text-font': [
-            'Open Sans Semibold',
-            'Arial Unicode MS Bold'
-          ],
-          'text-offset': [0, 1.25],
-          'text-anchor': 'top'
         }
       });
     }
   );
+});
+
+map.on('click', 'workshops', function (e) {
+  var coordinates = e.features[0].geometry.coordinates.slice();
+  var description = e.features[0].properties.description;
+
+  // Ensure that if the map is zoomed out such that multiple
+  // copies of the feature are visible, the popup appears
+  // over the copy being pointed to.
+  while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+  }
+
+  new mapboxgl.Popup()
+  .setLngLat(coordinates)
+  .setHTML(title + description)
+  .addTo(map);
 });
 
 // Change the cursor to a pointer when the mouse is over the places layer.
